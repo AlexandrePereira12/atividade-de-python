258b5c63257f5bbdb051ac1ad8c16c0fe5342435
Alexandre Pereira <116118540+AlexandrePereira12@users.noreply.github.com>
2023-03-17 20:24:05 -0300

    Add files via upload

---
+++ projetos.py
@@ -0,0 +1,28 @@
+#ex 01#
+
+a = input('Qual seu nome?')
+
+print(a)
+
+#ex 02#
+
+a1 = int(input('Qual foi a sua nota da AV1?'))
+
+a2 = int(input('Qual foi a sua nota da AV2?'))
+
+s = (a1+a2) /2
+
+if s>=6:
+    print('Você passou')
+else:
+    print('Você foi reprovado')
+
+#ex03#
+
+n1 = float(input('Digite um numero aleatorio: '))
+
+if n1>=10 and n1<=99:
+    print('True')
+else:
+    print('False')
+
